dbb96dba70d62aedab093ad615ab733dfd8b98bc
wocksl <wocksl1998@gmail.com>
2024-09-19 16:43:42 +0900

    fix alb dns

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://internal-backend-alb-882140738.ap-northeast-2.elb.amazonaws.com:3001',
+      target: 'http://internal-backend-alb-414631037.ap-northeast-2.elb.amazonaws.com:3001',
       changeOrigin: true,
     })
   );
